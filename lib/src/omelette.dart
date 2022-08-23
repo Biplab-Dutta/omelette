0d70d52b175ee42d4034e486e751d68493305209
Biplab Dutta <biplabdutta860@gmail.com>
2022-08-23 10:28:00 +0545

    chore: make linter happy

--- lib/src/omelette.dart
+++ lib/src/omelette.dart
@@ -1,7 +1,2 @@
-/// {@template omelette}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class Omelette {
-  /// {@macro omelette}
-  const Omelette();
-}
+export 'fresh.dart';
+export 'remote_response.dart';
